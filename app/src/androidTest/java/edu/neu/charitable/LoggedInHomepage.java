51f1a490c16353a984c6a07c464cf6c3b0821e6e
AriellesMacBook <arielleslatus@login.khoury.neu.edu>
2021-07-31 11:29:34 -0400

    set up activities and manifest

---
+++ app/src/androidTest/java/edu/neu/charitable/LoggedInHomepage.java
@@ -0,0 +1,14 @@
+package edu.neu.charitable;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoggedInHomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logged_in_homepage);
+    }
+}
