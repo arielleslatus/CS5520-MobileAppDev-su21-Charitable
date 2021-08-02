3816105549f971ae0353970502929c903dc639f3
Alice Tilles <tilles.al@husky.neu.edu>
2021-08-02 15:17:58 -0400

    Aligned button and rounded edges of image

--- app/src/main/java/edu/neu/charitable/CharityProfile.java
+++ app/src/main/java/edu/neu/charitable/CharityProfile.java
@@ -1,17 +1,23 @@
 package edu.neu.charitable;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 
 public class CharityProfile  extends AppCompatActivity {
 
     String charityName;
     String charityLocation;
     String charityMission;
+    int logoId;
 
 
     @Override
@@ -31,7 +37,11 @@ public class CharityProfile  extends AppCompatActivity {
         TextView charityMissionTextView = findViewById(R.id.charityMission);
         charityMissionTextView.setText(charityMission);
 
-
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), logoId);
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        roundedBitmapDrawable.setCircular(true);
+        imageView.setImageDrawable(roundedBitmapDrawable);
 
     }
 
@@ -58,6 +68,8 @@ public class CharityProfile  extends AppCompatActivity {
         charityName = "ASPCA";
         charityLocation = "Boston, MA, USA";
         charityMission = "Saving the lives of puppies and kittens since 1975.";
+        logoId = R.drawable.aspca_logo;
+
 
 
 
