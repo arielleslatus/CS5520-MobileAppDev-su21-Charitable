75552782a0f711b5897e604cf84a3a3858f85d8c
simpsonness <shanliness@gmail.com>
2021-08-09 18:08:45 +0100

    user profile update is working

--- app/src/main/java/edu/neu/charitable/RegisterUser.java
+++ app/src/main/java/edu/neu/charitable/RegisterUser.java
@@ -124,6 +124,7 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
                                 Toast.makeText(RegisterUser.this, "User has been " +
                                         "registered successfully!", Toast.LENGTH_LONG).show();
                                 progressBar.setVisibility(View.VISIBLE);
+                                progressBar.setVisibility(View.GONE);
                             } else {
                                 Toast.makeText(RegisterUser.this, "Failed to register!" +
                                         "Try again!", Toast.LENGTH_LONG).show();
